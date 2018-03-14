2fbd5380c74171d6c831b305f8c06c68ebcb262a
Anders Lund <anderslund41@gmail.com>
2018-03-14 12:04:08 +0100

    Added filter

--- src/App.js
+++ src/App.js
@@ -75,7 +75,8 @@ class Filter extends Component {
   render() {
     return (
       <div>
-        <input placeholder='Search'></input>
+        <input placeholder='Search' onKeyUp={event =>
+          this.props.onTextChange(event.target.value)}/>
       </div>
     )
   }
@@ -102,7 +103,10 @@ class Playlist extends Component {
 class App extends Component {
   constructor() {
     super();
-    this.state = {serverData: {}}
+    this.state = {
+      serverData: {},
+      filterString: '',
+    }
   }
   componentDidMount() {
     setTimeout(() => {
@@ -117,8 +121,11 @@ class App extends Component {
             <h2>{this.state.serverData.user.name}'s Playlist</h2>
             <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
             <HoursCounter playlists={this.state.serverData.user.playlists}/>
-            <Filter/>
-            {this.state.serverData.user.playlists.map(playlist =>
+            <Filter onTextChange={text => this.setState({filterString: text})}/>
+            {this.state.serverData.user.playlists.filter(playlist =>
+              playlist.name.toLowerCase().includes(
+                this.state.filterString.toLowerCase())
+            ).map(playlist =>
               <Playlist playlist={playlist}/>
             )}
 
